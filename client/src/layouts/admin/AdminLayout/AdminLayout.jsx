d9973545c6f068433bf9f9595968983ac0f454da
huytran2609 <20520551@gm.uit.edu.vn>
2023-12-04 14:57:47 +0700

    update file client

--- client/src/layouts/admin/AdminLayout/AdminLayout.jsx
+++ client/src/layouts/admin/AdminLayout/AdminLayout.jsx
@@ -1,12 +1,79 @@
 import PropTypes from 'prop-types';
 
-function AdminLayout({ children }) {
+import { useState } from 'react';
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme } from 'antd';
+const { Header, Sider, Content } = Layout;
+const AdminLayout = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
     return (
-        <div>
-            <div>{children}</div>
-        </div>
+        <Layout style={{height: "100%"}}>
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className="demo-logo-vertical" />
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            icon: <UserOutlined />,
+                            label: 'nav 1',
+                        },
+                        {
+                            key: '2',
+                            icon: <VideoCameraOutlined />,
+                            label: 'nav 2',
+                        },
+                        {
+                            key: '3',
+                            icon: <UploadOutlined />,
+                            label: 'nav 3',
+                        },
+                    ]}
+                />
+            </Sider>
+            <Layout>
+                <Header
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                >
+                    <Button
+                        type="text"
+                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '16px',
+                            width: 64,
+                            height: 64,
+                        }}
+                    />
+                </Header>
+                <Content
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                        background: colorBgContainer,
+                    }}
+                >
+                    Content
+                </Content>
+            </Layout>
+        </Layout>
     );
-}
+};
 
 AdminLayout.propTypes = {
     children: PropTypes.node.isRequired,
