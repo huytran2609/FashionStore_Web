30c43d139e09e0ad61c9690650f7acfe4d0d6d31
Thái Tăng Đức <thaitangduc1@gmail.com>
2024-01-01 01:01:41 +0700

    fixEditProfile

--- client/src/layouts/public/InputInformation/InputInformation.jsx
+++ client/src/layouts/public/InputInformation/InputInformation.jsx
@@ -1,18 +1,62 @@
+import { useState } from 'react'
 import styles from './InputInformation.module.scss'
 
 export default function InputInformation({ info, base, label1, label2, label3, label4, title, classTitle, classFullName, classPhone, classEmail, classAddress, readOnly }) {
-    console.log(info?.name)
+    const [nameValue, setNameValue] = useState(info?.name);
+    const [emailValue, setEmailValue] = useState(info?.email);
+    const [phoneValue, setPhoneValue] = useState(info?.phone);
+    const [addressValue, setAddressValue] = useState(info?.address);
+    const handleChangeName = (e) => {
+        setNameValue(e.target.value)
+    }
+    const handleChangeEmail = (e) => {
+        setEmailValue(e.target.value)
+    }
+    const handleChangePhone = (e) => {
+        setPhoneValue(e.target.value)
+    }
+    const handleChangeAddress = (e) => {
+        setAddressValue(e.target.value)
+    }
     return (
         <>
             <h1 className={`${classTitle} ${styles.title}`}>{title}</h1>
             <span>{label1}</span>
-            <input style={readOnly ? { backgroundColor: '#e5e5e5', color: '#000' } : {}} className={`${classFullName} ${base} ${styles.baseInput}`} value={info?.name} type="text" placeholder='FullName...' readOnly={readOnly} />
+            <input
+                style={readOnly ? { backgroundColor: '#e5e5e5', color: '#000', userSelect: 'none' } : {}}
+                className={`${classFullName} ${base} ${styles.baseInput}`}
+                onChange={handleChangeName}
+                value={nameValue}
+                type="text"
+                placeholder='FullName...'
+                readOnly={readOnly} />
             <span>{label2}</span>
-            <input style={readOnly ? { backgroundColor: '#e5e5e5', color: '#000' } : {}} className={`${classPhone} ${base} ${styles.baseInput}`} value={info?.phone} type="tel" placeholder='Phone number...' readOnly={readOnly} />
+            <input
+                style={readOnly ? { backgroundColor: '#e5e5e5', color: '#000', userSelect: 'none' } : {}}
+                className={`${classPhone} ${base} ${styles.baseInput}`}
+                onChange={handleChangePhone}
+                value={phoneValue}
+                type="tel"
+                placeholder='Phone number...'
+                readOnly={readOnly} />
             <span>{label3}</span>
-            <input style={readOnly ? { backgroundColor: '#e5e5e5', color: '#000' } : {}} className={`${classEmail} ${base} ${styles.baseInput}`} value={info?.email} type="email" placeholder='Email...' readOnly={readOnly} />
+            <input
+                style={readOnly ? { backgroundColor: '#e5e5e5', color: '#000', userSelect: 'none' } : {}}
+                className={`${classEmail} ${base} ${styles.baseInput}`}
+                onChange={handleChangeEmail}
+                value={emailValue}
+                type="email"
+                placeholder='Email...'
+                readOnly={readOnly} />
             <span>{label4}</span>
-            <input style={readOnly ? { backgroundColor: '#e5e5e5', color: '#000' } : {}} className={`${classAddress} ${base} ${styles.baseInput}`} value={info?.address} type="text" placeholder='Address...' readOnly={readOnly} />
+            <input
+                style={readOnly ? { backgroundColor: '#e5e5e5', color: '#000', userSelect: 'none' } : {}}
+                className={`${classAddress} ${base} ${styles.baseInput}`}
+                onChange={handleChangeAddress}
+                value={addressValue}
+                type="text"
+                placeholder='Address...'
+                readOnly={readOnly} />
         </>
     )
 }
