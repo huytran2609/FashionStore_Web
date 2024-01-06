5bf86a2edbf82e8f35aceba544f0bad1c7570410
huytran2609 <20520551@gm.uit.edu.vn>
2024-01-06 17:12:24 +0700

    update server to deploy

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ const cors = require('cors');
 const app = express();
 app.use(
     cors({
-        origin: process.env.CLIENT_URL,
-        // origin: '*',
+        // origin: [process.env.CLIENT_URL, process.env.CLIENT_URL2],
+        origin: '*',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
     }),
