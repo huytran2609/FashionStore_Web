4552b09c4da9455718a83cbd0b8d46355549be94
Thái Tăng Đức <thaitangduc1@gmail.com>
2025-06-23 21:17:30 +0700

    update-test-UI

--- client/src/layouts/public/Header/Header.jsx
+++ client/src/layouts/public/Header/Header.jsx
@@ -19,139 +19,139 @@ import { HiClipboardDocumentList } from "react-icons/hi2";
 
 export default function Header({ handleInputChange, query }) {
 
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const { isLoggedIn, current, isToastVisible } = useSelector((state) => state.user);
-    const [categories, setCategories] = useState([]);
-    useEffect(() => {
-        let timeoutId;
-        if (isLoggedIn) {
-            dispatch(getCurrent());
+	const { isLoggedIn, current, isToastVisible } = useSelector((state) => state.user);
+	const [categories, setCategories] = useState([]);
+	useEffect(() => {
+		let timeoutId;
+		if (isLoggedIn) {
+			dispatch(getCurrent());
 
-            if (isToastVisible) {
-                toast.success('Logged in Successfully!');
-                timeoutId = setTimeout(() => {
-                    dispatch(setToastVisibility());
-                }, 100);
-            }
-        }
-        return () => {
-            if (timeoutId) {
-                clearTimeout(timeoutId);
-            }
-        };
-    }, [dispatch, isLoggedIn, isToastVisible]);
+			if (isToastVisible) {
+				toast.success('Logged in Successfully!');
+				timeoutId = setTimeout(() => {
+					dispatch(setToastVisibility());
+				}, 100);
+			}
+		}
+		return () => {
+			if (timeoutId) {
+				clearTimeout(timeoutId);
+			}
+		};
+	}, [dispatch, isLoggedIn, isToastVisible]);
 
-    const displayName = current?.name;
-    const firstName = displayName ? displayName.split(' ')[0] : '';
+	const displayName = current?.name;
+	const firstName = displayName ? displayName.split(' ')[0] : '';
 
-    useEffect(() => {
-        const fetchApiCategories = async () => {
-            const response = await categoryApi.getAll();
-            setCategories(response.dataCategories);
-        };
-        fetchApiCategories();
-    }, []);
+	useEffect(() => {
+		const fetchApiCategories = async () => {
+			const response = await categoryApi.getAll();
+			setCategories(response.dataCategories);
+		};
+		fetchApiCategories();
+	}, []);
 
-    return (
-        <>
-            {isLoggedIn ? <ChatBot /> : ''}
-            <Row className={styles.header}>
-                <Col span={3} className={styles.logo}>
-                    <Link to={config.home}>
-                        <img src={Logo} alt="Logo" />
-                    </Link>
-                </Col>
-                <div className={styles.lineHeader}></div>
-                <Col span={11} className={styles.category}>
-                    <ul className={styles.categoryList}>
-                        {categories.map((data) => (
-                            <Link key={data?._id} to={config[data.title.toLowerCase()]}>
-                                <li>{data?.title}</li>
-                            </Link>
-                        ))}
-                    </ul>
-                </Col>
-                <Col className={styles.cpnHeader3} span={10}>
-                    <div className={styles.headerSearch}>
-                        <FaSearch className={styles.searchIcon} />
-                        <input onChange={handleInputChange}
-                            value={query} type="text" placeholder="Enter to Search ..." className={styles.searchInput} />
-                    </div>
+	return (
+		<>
+			{isLoggedIn ? <ChatBot /> : ''}
+			<Row className={styles.header}>
+				<Col span={3} className={styles.logo}>
+					<Link to={config.home}>
+						<img src={Logo} alt="Logo" />
+					</Link>
+				</Col>
+				<div className={styles.lineHeader}></div>
+				<Col span={11} className={styles.category}>
+					<ul className={styles.categoryList}>
+						{categories.map((data) => (
+							<Link key={data?._id} to={config[data.title.toLowerCase()]}>
+								<li style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '90px', height: '40px', lineHeight: '40px', backgroundColor: '#547cbe', borderRadius: '40px', color: '#ffffff', boxShadow: '0 1px 3px #858585' }}>{data?.title}</li>
+							</Link>
+						))}
+					</ul>
+				</Col>
+				<Col className={styles.cpnHeader3} span={10}>
+					<div className={styles.headerSearch}>
+						<FaSearch className={styles.searchIcon} />
+						<input onChange={handleInputChange}
+							value={query} type="text" placeholder="Enter to Search ..." className={styles.searchInput} />
+					</div>
 
-                    <Link to={config.cart} className={styles.cart}>
-                        <FaShoppingCart className={styles.cartIcon} />
-                        <div className={styles.bgNumCart}>
-                            <label>{current?.cart?.length || 0}</label>
-                        </div>
-                    </Link>
+					<Link to={config.cart} className={styles.cart}>
+						<FaShoppingCart className={styles.cartIcon} />
+						<div className={styles.bgNumCart}>
+							<label>{current?.cart?.length || 0}</label>
+						</div>
+					</Link>
 
-                    {!isLoggedIn ? (
-                        <>
-                            <Button link={config.login} content="Login" />
+					{!isLoggedIn ? (
+						<>
+							<Button link={config.login} content="Login" />
 
-                            <Button link={config.register} content="Register" />
-                        </>
-                    ) : (
-                        <div className={styles.userInfo}>
-                            <h3>Hi,&nbsp;{firstName}</h3>
-                            <Link to={config.profile} className={styles.imgAvatar}>
-                                <img src={avatar} alt="UserImg" />
-                            </Link>
-                            <div className={styles.dropdownContent}>
-                                <Link
-                                    style={{
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'space-between',
-                                        color: 'black',
-                                        fontSize: '15px',
-                                    }}
-                                    to={config.profile}
-                                >
-                                    Profile <ImProfile />
-                                </Link>
-                                <Link
-                                    style={{
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'space-between',
-                                        color: 'black',
-                                        fontSize: '15px',
-                                    }}
-                                    to={config.history}
-                                >
-                                    My Order <HiClipboardDocumentList />
-                                </Link>
-                                <Link
-                                    style={{
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'space-between',
-                                        color: 'black',
-                                        fontSize: '15px',
-                                    }}
-                                    to={config.category}
-                                >
-                                    Category <BiSolidCategoryAlt />
-                                </Link>
-                                <Link
-                                    onClick={() => dispatch(logout())}
-                                    style={{
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'space-between',
-                                        color: 'red',
-                                        fontSize: '15px',
-                                    }}
-                                >
-                                    Logout <FaSignOutAlt />
-                                </Link>
-                            </div>
-                        </div>
-                    )}
-                </Col>
-            </Row>
-        </>
-    );
+							<Button link={config.register} content="Register" />
+						</>
+					) : (
+						<div className={styles.userInfo}>
+							<h3>Hi,&nbsp;{firstName}</h3>
+							<Link to={config.profile} className={styles.imgAvatar}>
+								<img src={avatar} alt="UserImg" />
+							</Link>
+							<div className={styles.dropdownContent}>
+								<Link
+									style={{
+										display: 'flex',
+										alignItems: 'center',
+										justifyContent: 'space-between',
+										color: 'black',
+										fontSize: '15px',
+									}}
+									to={config.profile}
+								>
+									Profile <ImProfile />
+								</Link>
+								<Link
+									style={{
+										display: 'flex',
+										alignItems: 'center',
+										justifyContent: 'space-between',
+										color: 'black',
+										fontSize: '15px',
+									}}
+									to={config.history}
+								>
+									My Order <HiClipboardDocumentList />
+								</Link>
+								<Link
+									style={{
+										display: 'flex',
+										alignItems: 'center',
+										justifyContent: 'space-between',
+										color: 'black',
+										fontSize: '15px',
+									}}
+									to={config.category}
+								>
+									Category <BiSolidCategoryAlt />
+								</Link>
+								<Link
+									onClick={() => dispatch(logout())}
+									style={{
+										display: 'flex',
+										alignItems: 'center',
+										justifyContent: 'space-between',
+										color: 'red',
+										fontSize: '15px',
+									}}
+								>
+									Logout <FaSignOutAlt />
+								</Link>
+							</div>
+						</div>
+					)}
+				</Col>
+			</Row>
+		</>
+	);
 }
